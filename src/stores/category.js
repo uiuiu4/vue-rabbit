690c01e36ac6209188e932c87b632df43f929eed
傅嘉愉 <1923332681@qq.com>
2026-08-10 21:41:24 +0800

    pinia优化导航数据

---
+++ src/stores/category.js
@@ -0,0 +1,16 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/api/layout'
+
+export const useCategoryStore = defineStore('category', () => {
+  //state 导航列表数据
+  const categoryList = ref([])
+
+  //action 获取导航数据的方法
+  const getCategory = async()=>{
+    const res = await getCategoryAPI()
+    categoryList.value = res.result
+  }
+
+  return { categoryList , getCategory }
+})
