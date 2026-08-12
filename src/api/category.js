c15f6132fe81f7e9d30c7471dc37e2f3e528f302
傅嘉愉 <1923332681@qq.com>
2026-08-12 20:50:42 +0800

    二级分类-面包屑导航实现

--- src/api/category.js
+++ src/api/category.js
@@ -1,9 +1,19 @@
 import request from '@/utils/http'
+import { id } from 'element-plus/es/locales.mjs'
 
 export function getCategoryAPI(id){
   return request({
     url: '/category',
     params: {
+      id
+    }
+  })
+}
+
+export function getCategoryFilterAPI(id){
+  return request({
+    url:'/category/sub/filter',
+    params:{
       id
     }
   })
