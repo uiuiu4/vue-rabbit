599f09c4d838e6cd90181b3bd244e4396035a538
傅嘉愉 <1923332681@qq.com>
2026-08-12 13:59:57 +0800

    激活状态显示以及分类列表渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -17,7 +17,7 @@
           <RouterLink to="/">首页</RouterLink>
         </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
@@ -129,6 +129,10 @@
         border-radius: 10px;
         font-family: Arial;
       }
+    }
+    .active{
+      color: $xtxColor;
+      border-bottom: 1px solid $xtxColor;
     }
   }
 }
