67ee7c7dbf55af82b442043433aeb55aee7d9570
傅嘉愉 <1923332681@qq.com>
2026-08-16 23:20:29 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -12,10 +12,15 @@ export const useUserStore = defineStore('user',()=>{
     const res = await loginAPI({account,password})
     userInfo.value = res.result
   }
+  //退出时清除用户数据
+  const clearUserInfo = () =>{
+    userInfo.value = {}
+  }
   //以对象格式把state和action return
   return{
     userInfo,
-    getUserInfo
+    getUserInfo,
+    clearUserInfo
   }
 },{
   persist: true,
