09b4e6bf56b775d222458a140fcedb954c5b08fd
傅嘉愉 <1923332681@qq.com>
2026-08-16 23:06:57 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
-import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
 const httpInstance = axios.create({
   baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -11,6 +11,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  //1.从pinia获取token数据
+  const userStore = useUserStore()
+  //2.按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
