e677581a35cb281d5441d64d6a71e0f223aeb191
傅嘉愉 <1923332681@qq.com>
2026-08-19 20:09:37 +0800

    倒计时函数

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -2,17 +2,20 @@
 import { getOrderAPI } from '@/api/pay'
 import {onMounted, ref} from 'vue'
 import { useRoute } from 'vue-router';
+import { useCountDown } from '@/composables/useCountDown';
 
+const { formatTime, start } = useCountDown()
 //获取订单数据
 const payInfo = ref({})
 const route = useRoute()
 const getPayInfo = async() => {
   const res = await getOrderAPI(route.query.id)
   payInfo.value = res.result
+  //初始化倒计时秒数
+  start(res.result.countdown)
 }
 
 onMounted(()=>getPayInfo())
-
 //跳转支付
 //携带订单id以及回调地址跳转到支付地址（get）
 // 支付地址
@@ -31,7 +34,7 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
